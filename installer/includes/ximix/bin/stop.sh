982803747c6663a5c23a9f701eece30afcf2e45a
Megan Woods <megan.woods@woods-gebler.com>
2013-08-12 13:55:03 +1000

    Added stop and sysV startup script to installer directory

---
+++ installer/includes/ximix/bin/stop.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ -z "$XIMXI_HOME" ]]; then
+        L=`dirname $0`
+        XIMIX_HOME="$L/../"
+
+fi
+
+if [[ ! -f "$XIMIX_HOME/libs/node.jar" ]]; then
+        echo "Could not find libs/node.jar off XIMIX_HOME ( $XIMIX_HOME )"
+        exit -1
+fi
+
+PIDFILE="$XIMIX_HOME/$1/$1.pid"
+
+if [[ ! -f "$PIDFILE" ]]; then
+	echo "Pid file not found: $PIDFILE";
+	exit -1;
+fi
+
+PID=`cat $PIDFILE`
+
+if ps -p $PID > /dev/null; then
+COUNTER=12
+until [  $COUNTER -lt 0 ]; do
+	echo "Sending kill -15 to $1 ($PID)"
+	kill -15 $PID
+	sleep 5;
+	let COUNTER-=1
+	if ps -p $PID >/dev/null; then
+	continue;
+	fi
+	echo "Stopped node $1";
+	exit 0;
+done
+
+echo "$1 not responding to kill -15, sending kill -9"
+
+kill -9 $PID
+else
+    echo "$1 with pid $PID is not running."
+fi
+
---
+++ installer/includes/ximix/bin/ximix_sys_v.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#
+# chkconfig: 35 90 12
+# description: Ximix Node
+#
+
+# Define the node.
+#
+NODE=node1
+#
+
+#
+# The user to run ximix under.
+
+if [[ "x$XIMIX_USER" = "x" ]]; then
+	XIMIX_USER=ximix
+fi
+
+
+#
+# Where to direct stdout and stderr
+#
+if [[ "x$XIMIX_CONSOLE" = "x" ]]; then
+	XIMIX_CONSOLE="/dev/null"
+fi
+
+
+# export JAVA_HOME=/path/to/java
+# export XIMIX_HOME=/path/to/ximix
+
+
+if [[ "x$JAVA_HOME" = "x" ]]; then
+	echo "JAVA_HOME is not defined."
+    exit -1
+fi
+
+if [[ "x$XIMIX_HOME" = "x" ]]; then
+        echo "XIMIX_HOME is not defined."
+        exit -1
+fi
+
+case "$1" in
+  start)
+        su $XIMIX_USER -c "$XIMIX_HOME/bin/start.sh $NODE 2>&1" > $XIMIX_CONSOLE
+        ;;
+  stop)
+        su $XIMIX_USER -c "$XIMIX_HOME/bin/stop.sh $NODE"
+        ;;
+  restart)
+        stop
+        start
+        ;;
+  *)
+        echo $"Usage: $0 {start|stop|restart}"
+        exit 1
+esac
+exit 0
+
